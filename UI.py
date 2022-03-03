2c228c43173e8484eb8cf5daacd54097fc4184ec
blake <blakegabb@gmail.com>
2022-03-03 13:20:53 +1300

    this files not even used at all...

--- UI.py
+++ UI.py
@@ -1,6 +1,6 @@
 #this is meant to handle all functions that will effect the interface, like fuel, mono ETC
 #not sure how pygame reacts to functions from imports, but one way to find out
-
+#this file isnt in use, just yet but who knows
 import pygame
 
 
